6a46f1a4c199007a8fe09ba977db36851e991074
norkfy <lyxd1@icloud.com>
2017-03-03 14:24:32 +0300

    Add timeout option

--- src/nwjs-autoupdater/main.go
+++ src/nwjs-autoupdater/main.go
@@ -14,13 +14,17 @@ import (
 
 func main() {
 	var bundle, instDir, appName string
+	var timeout int
 
-	time.Sleep(1000 * time.Millisecond)
+	time.Sleep(2000 * time.Millisecond)
 
 	flag.StringVar(&bundle, "bundle", "", "Path to the update package")
 	flag.StringVar(&instDir, "inst-dir", "", "Path to the application install dir")
 	flag.StringVar(&appName, "app", "", "Application name")
+	flag.IntVar(&timeout, "timeout", 0, "Delay before copying files")
 	flag.Parse()
+
+	time.Sleep(time.Duration(timeout) * time.Millisecond)
 
 	cwd, _ := os.Getwd()
 	logfile, err := os.Create(filepath.Join(cwd, "updater.log"))
--- src/nwjs-autoupdater/updater/updater_windows.go
+++ src/nwjs-autoupdater/updater/updater_windows.go
@@ -2,10 +2,6 @@ package updater
 
 import (
 	"path/filepath"
-	"os"
-	"fmt"
-	"io"
-	"io/ioutil"
 
 	"github.com/mholt/archiver"
 )
@@ -14,125 +10,10 @@ func Update(bundle, instDir, appName string) (error, string) {
 	appExecName := appName + ".exe"
   appExec := filepath.Join(instDir, appExecName)
 
-	tempDir := os.TempDir() + "/" + appName
-
-  err := archiver.Zip.Open(bundle, os.TempDir())
-	if err != nil {
-		return err, appExec
-	}
-
-	err = os.RemoveAll(instDir)
-	if err != nil {
-		return err, appExec
-	}
-
-	err = copyDir(tempDir, instDir)
+  err := archiver.Zip.Open(bundle, instDir)
 	if err != nil {
 		return err, appExec
 	}
 
   return nil, appExec
-}
-
-
-// CopyFile copies the contents of the file named src to the file named
-// by dst. The file will be created if it does not already exist. If the
-// destination file exists, all it's contents will be replaced by the contents
-// of the source file. The file mode will be copied from the source and
-// the copied data is synced/flushed to stable storage.
-func copyFile(src, dst string) (err error) {
-	in, err := os.Open(src)
-	if err != nil {
-		return
-	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		return
-	}
-	defer func() {
-		if e := out.Close(); e != nil {
-			err = e
-		}
-	}()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return
-	}
-
-	err = out.Sync()
-	if err != nil {
-		return
-	}
-
-	si, err := os.Stat(src)
-	if err != nil {
-		return
-	}
-	err = os.Chmod(dst, si.Mode())
-	if err != nil {
-		return
-	}
-
-	return
-}
-
-// CopyDir recursively copies a directory tree, attempting to preserve permissions.
-// Source directory must exist, destination directory must *not* exist.
-// Symlinks are ignored and skipped.
-func copyDir(src string, dst string) (err error) {
-	src = filepath.Clean(src)
-	dst = filepath.Clean(dst)
-
-	si, err := os.Stat(src)
-	if err != nil {
-		return err
-	}
-	if !si.IsDir() {
-		return fmt.Errorf("source is not a directory")
-	}
-
-	_, err = os.Stat(dst)
-	if err != nil && !os.IsNotExist(err) {
-		return
-	}
-	if err == nil {
-		return fmt.Errorf("destination already exists")
-	}
-
-	err = os.MkdirAll(dst, si.Mode())
-	if err != nil {
-		return
-	}
-
-	entries, err := ioutil.ReadDir(src)
-	if err != nil {
-		return
-	}
-
-	for _, entry := range entries {
-		srcPath := filepath.Join(src, entry.Name())
-		dstPath := filepath.Join(dst, entry.Name())
-
-		if entry.IsDir() {
-			err = copyDir(srcPath, dstPath)
-			if err != nil {
-				return
-			}
-		} else {
-			// Skip symlinks.
-			if entry.Mode()&os.ModeSymlink != 0 {
-				continue
-			}
-
-			err = copyFile(srcPath, dstPath)
-			if err != nil {
-				return
-			}
-		}
-	}
-
-	return
 }
