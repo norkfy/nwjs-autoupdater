488e56f7d143dc455859ed41d51cb0d5bef521f9
norkfy <lyxd1@icloud.com>
2017-03-22 14:45:01 +0300

    Removing source code path

--- src/nwjs-autoupdater/updater/updater_windows.go
+++ src/nwjs-autoupdater/updater/updater_windows.go
@@ -2,7 +2,7 @@ package updater
 
 import (
 	"path/filepath"
-
+	"os"
 	"github.com/mholt/archiver"
 )
 
@@ -10,7 +10,13 @@ func Update(bundle, instDir, appName string) (error, string) {
 	appExecName := appName + ".exe"
   appExec := filepath.Join(instDir, appExecName)
 
-  err := archiver.Zip.Open(bundle, instDir)
+	err := os.RemoveAll(filepath.Join(instDir, "package.nw"));
+
+	if err != nil {
+		return err, appExec
+	}
+
+  err = archiver.Zip.Open(bundle, instDir)
 	if err != nil {
 		return err, appExec
 	}
